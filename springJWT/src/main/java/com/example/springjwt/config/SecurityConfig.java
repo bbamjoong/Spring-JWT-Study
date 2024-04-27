b6501c1faab7a2d0ab0b99720f99ec9012d11dec
김범중 <rlaqjawnd625@naver.com>
2024-04-27 21:51:23 +0900

    add Chapter1-3 Code

---
+++ springJWT/src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.springjwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // BCrypt Password Encoder 객체를 생성하여 반환하는 Bean 생성
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //csrf disable
+        /**
+         * session 방식은 session이 고정적이기 때문에 csrf 공격을 방어해주어야 한다.
+         * JWT는 session을 Stateless 상태로 관리하기 때문에 disable로 둔다.
+         */
+        http
+                .csrf((auth) -> auth.disable());
+
+        /**
+         * JWT를 쓰므로 formLogin, httpBasic 인증 방식 또한 disable 해준다.
+         */
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+        //http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+        /**
+         * session을 stateless한 상태로 관리하기 위해 해당 설정을 해준다.
+         */
+        //세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}

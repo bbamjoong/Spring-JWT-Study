29ae6e53555dfe69e31ed771c706df004eb515a7
김범중 <rlaqjawnd625@naver.com>
2024-04-28 20:46:29 +0900

    add Chapter12 code

--- springJWT/src/main/java/com/example/springjwt/controller/MainController.java
+++ springJWT/src/main/java/com/example/springjwt/controller/MainController.java
@@ -1,5 +1,10 @@
 package com.example.springjwt.controller;
 
+import java.util.Collection;
+import java.util.Iterator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +16,17 @@ public class MainController {
     @GetMapping("/")
     public String mainP() {
 
-        return "main Controller";
+        // 세션 현재 사용자 아이디
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // 세션 현재 사용자 role
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        return "main Controller" + " " + username + " " + role;
     }
 }
